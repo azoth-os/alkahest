de777a9842efbd32354506b98e06cdd1e3e11870
jclermonttt <jer.clermont@gmail.com>
2025-12-31 00:32:33 -0500

    add crate for session real-time

---
+++ alkahest-session/src/lib.rs
@@ -0,0 +1,39 @@
+#![cfg_attr(not(test), no_std)]
+
+pub enum SessionState {
+    ///
+    Validating,
+    ///
+    Compiling,
+    ///
+    Linking,
+    ///
+    Ready,
+    ///
+    Failed,
+}
+
+pub struct Session {
+    pub module_id: u32,
+    state: SessionState,
+}
+
+impl Session {
+    pub fn new(module_id: u32) -> Self {
+        Self {
+            module_id,
+            state: SessionState::Validating,
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn new_session() {
+        let _ = Session::new(123);       
+    }
+}
