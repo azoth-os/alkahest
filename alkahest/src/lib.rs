d455cb2c0496f9794745e8a6d73b540ae4faa83c
jclermonttt <jer.clermont@gmail.com>
2026-01-01 03:36:37 -0500

    add crate for macros and add struct for the interface of engine

--- alkahest/src/lib.rs
+++ alkahest/src/lib.rs
@@ -1,27 +1,9 @@
 //! # Alkahest - Hypervisor Type-1
 #![cfg_attr(not(test), no_std)]
 
-/// a crucible is an execution context for the [Alkahest].
+
+pub struct AlkahestEngine<C: Crucible> {
+    kernel: C
+}
+
 pub trait Crucible {}
-
-#[allow(unused)]
-pub struct Engine<C: Crucible> {
-    context: C,
-}
-
-impl<C: Crucible> Engine<C> {
-    pub fn new(context: C) -> Self {
-        Self { context }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    struct TestCrucible;
-    impl Crucible for TestCrucible {}
-    #[test]
-    pub fn new_engine() {
-        let _ = Engine::new(TestCrucible);
-    }
-}
