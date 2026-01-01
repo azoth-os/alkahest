8777774a0be398280f96c53e558906cd785d93ad
jclermonttt <jer.clermont@gmail.com>
2026-01-01 02:38:24 -0500

    add base for api

--- alkahest/src/lib.rs
+++ alkahest/src/lib.rs
@@ -1,6 +1,27 @@
 //! # Alkahest - Hypervisor Type-1
 #![cfg_attr(not(test), no_std)]
 
-pub mod prelude;
-#[allow(unused_imports)]
-pub use crate::prelude::*;
+/// a crucible is an execution context for the [Alkahest].
+pub trait Crucible {}
+
+#[allow(unused)]
+pub struct Engine<C: Crucible> {
+    context: C,
+}
+
+impl<C: Crucible> Engine<C> {
+    pub fn new(context: C) -> Self {
+        Self { context }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    struct TestCrucible;
+    impl Crucible for TestCrucible {}
+    #[test]
+    pub fn new_engine() {
+        let _ = Engine::new(TestCrucible);
+    }
+}
